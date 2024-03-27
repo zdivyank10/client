9e8af4dd10f333a2c8f956539ab3f0e4405948d8
zdivyank <divyankzaveri10@gmail.com>
2024-03-27 15:08:09 +0530

    popular blogs working

--- client/src/components/blog/Famous.jsx
+++ client/src/components/blog/Famous.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../store/auth';
-import DOMPurify from 'dompurify';
+import './famous.css'; // Import CSS file for custom styles
+import Carousel from 'react-bootstrap/Carousel'; // Import the Carousel component
+import DOMPurify from 'dompurify'; // Import DOMPurify for sanitization
 
 function Famous() {
   const { API_BASE_URL } = useAuth();
@@ -25,29 +27,37 @@ function Famous() {
   }, []);
 
   return (
-    <Row xs={1} md={2} lg={5} className="g-4 m-3" style={{ display: 'flex'}} >
-      {popularblog && popularblog.map((post, index) => {
-        const { _id, title, cover_img, content, createdAt,author_id } = post;
-        const sanitizedContent = DOMPurify.sanitize(content);
-        return (
-          <Col key={_id}>
-            <Card style={{ height: '100%' }}>
-              <Card.Img variant="top" src={`${API_BASE_URL}uploads/${cover_img}`} />
-              <Card.Body>
-                <Card.Title>{author_id.username}</Card.Title>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                  <div className="content" dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">{createdAt}</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-        )
-      })}
-    </Row>
+    <div className="popular_container">
+      <Carousel data-bs-theme="light">
+        {/* Map over the popularblog state to dynamically render Carousel items */}
+        {popularblog.map((blog, index) => (
+          <Carousel.Item key={index}>
+            <Link to={`/blog/${blog._id}`} className="postuserinfo">
+              {/* <div className="darker">
+                <img
+                  className="d-block w-100 pop_img text-center"
+                  src={`${API_BASE_URL}uploads/${blog.cover_img}`}
+                  alt={blog.title}
+                />
+              </div> */}
+              <div class="img-gradient">
+                <img className=" w-100 pop_img "
+                  src={`${API_BASE_URL}uploads/${blog.cover_img}`} />
+              </div>
+              <Carousel.Caption className=' d-block'>
+                <h5 className='text-light'>{blog.title}</h5>
+                <p className="pop_content" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog.content) }} />
+                {blog.tags.map((tag, tagIndex) => (
+                        <span key={tagIndex} className="tag text-dark">
+                          {tag}
+                        </span>
+                      ))}
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }
 
